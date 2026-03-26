14e56c738dad726537af7d7a914c3c2d24bda5df
Rohit Barma <barmarohit05@gmail.com>
2026-03-26 22:43:20 +0530

    Implement user registration and login functionality with bcrypt for password hashing

--- backend-side/src/controllers/user.controller.js
+++ backend-side/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 const registerUser = async(req, res) => {
     // we take data from the request body
@@ -28,14 +29,72 @@ const registerUser = async(req, res) => {
             avatar: " "
         })
 
-        return res.status(201).json({status: 201, success: true, message: "User created successfully!", data: newUser}) 
+        const userResponse = {
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+            avatar: newUser.avatar
+        }
+
+        return res.status(201).json({
+            status: 201, 
+            success: true, 
+            message: "User created successfully!", 
+            data: userResponse
+        }) 
 
     } catch (error) {
         console.log(error);
-        return res.status(500).json({status: 500, success: false, message: "Internal server error!"})   
+        return res.status(500).json({
+            status: 500, 
+            success: false, 
+            message: "Internal server error!"
+        })   
         
     }
 
 }
 
-export {registerUser}
+const loginUser = async(req, res) => {
+    // we will take data from the request body
+    // we will check user email exist or not
+    // if email not exist when we will send error message
+    // if email exist then we will compare password
+    // if password not match then we will send error message
+    // if password match then we will send response to client
+    // we will generate token and send to client
+    // we will save token to database
+    // we will set token in cookie
+    // we will send response to client
+
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({status: 400, success: false, message: "Please enter all fields!"}) 
+        }
+
+        const isUserExist = await User.findOne({email});
+        if(!isUserExist){
+            return res.status(400).json({status: 400, success: false, message: "User not exist!"}) 
+        }
+
+        const isPasswordMatch = await isUserExist.comparePassword(password);
+        if(!isPasswordMatch){
+            return res.status(400).json({status: 400, success: false, message: "Invalid credentials!"})
+        }
+
+        const userResponse = {
+            _id: isUserExist._id,
+            name: isUserExist.name,
+            email: isUserExist.email,
+            avatar: isUserExist.avatar
+        }
+
+        return res.status(200).json({status: 200, success: true, message: "User logged in successfully!", data: userResponse})
+
+    } catch (error) {
+        res.status(500).json({status: 500, success: false, message: "Internal server error!"})
+    }
+}
+
+export {registerUser, loginUser}
--- backend-side/src/models/user.model.js
+++ backend-side/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema({
     name: {
@@ -20,8 +21,20 @@ const userSchema = new Schema({
     },
     avatar: {
         type: String,
+        default: ""
     }
 }, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 
